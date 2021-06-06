fe1c0bee722c91ea7773e2798554be592f338fa0
Sergio Ananias <sergio.ananias.o@gmail.com>
2021-06-06 15:36:07 -0400

    Primer commit gold clicker, falta agregar condicion de victoria

---
+++ app/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from random import randint
+
+
+
+def index(request):
+    if not 'gold' in request.session or not request.session['gold']:
+        request.session['gold'] = 0
+    if not 'message_list' in request.session or not request.session['message_list']:
+        request.session['message_list'] = []
+    context = {
+        'gold' : request.session['gold'],
+    }
+    return render(request, 'index.html', context)
+
+
+def money(request):
+    request.session['value'] = request.POST['processmoney']
+
+
+    if request.session['value'] == 'Farm':
+
+        numero = randint(10, 20)
+        request.session['gold'] += numero
+        request.session['message_list'].insert(0,numero)
+
+
+    elif request.session['value'] == 'Cave':
+
+        numero = randint(5, 10)
+        request.session['gold'] += numero
+        request.session['message_list'].insert(0,numero)
+
+
+    elif request.session['value'] == 'House':
+
+        numero = randint(2, 5)
+        request.session['gold'] += numero
+        request.session['message_list'].insert(0,numero)
+
+
+    elif request.session['value'] == 'Casino':
+
+        numero = randint(-50, 50)
+        request.session['gold'] += numero
+        request.session['message_list'].insert(0,numero)
+
+    request.session.save()
+
+    return redirect('/')
+
+def reset(request):
+    request.session['gold'] = 0
+    request.session['message_list'] = []
+    return redirect('/')
+    
+def victoria(request):
+    request.session['gold'] = 0
+    request.session['message_list'] = []
+    request.session['victoria'] = 0
+    return render(request, 'victoria.html')
+# Create your views here.
